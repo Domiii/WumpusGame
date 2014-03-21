cb071a255af8232f03c83b3feed6321692e0546e
domiii <dominik.seifert@gmail.com>
2014-03-21 14:19:38 +0800

    - minor changes

--- js/ui/GridDOM.js
+++ js/ui/GridDOM.js
@@ -6,31 +6,32 @@
  /**
   * A uniform, dense grid with the given amount of tiles.
   *
-  * @param {Object} config The Grid configuration.
-  * @param {Element} [config.gridEl] The underlying DOM element of the grid.
+  * @param {Object} config The MapView component and settings.
+  * @param {Element} [config.gridEl] The actual element of the grid.
   * @param {Element} [config.tileElTemplate] The underlying DOM element of a grid tile. All tile elements will be copies of this element.
-  * @param {Element} [config.width] The number of tiles in the horizontal direction.
+  * @param {Element} [config.grid] The actual grid.
   * @param {Element} [config.height] The number of tiles in the vertical direction.
   */
-searchGame.Grid = function(config) {
-    // shallow-copy config options into this object
-    config.clone(false, this);
-    
+searchGame.makeGrid = function(config) {
 	// sanity checks
     squishy.assert(config.gridEl, "config.gridEl is not defined.");
+    squishy.assert(config.grid, "config.grid is not defined.");
     squishy.assert(config.tileElTemplate, "config.tileElTemplate is not defined.");
-    squishy.assert(config.width > 0, "config.width is not a number greater than zero");
-    squishy.assert(config.height > 0, "config.height is not a number greater than zero");
+    
+    var el = config.gridEl;
+    
+    // shallow-copy config options into the grid element.
+    config.clone(false, el);
 	
 	// create tiles
 	this.reset();
 };
 
 /** 
- * Creates the underlying grid. Removes all existing tiles and other objects within the grid.
+ * Creates the underlying DOM. Removes all existing tiles and other objects within the grid.
  * WARNING: Do not call this, unless you are sure that your grid is not currently in use anywhere else.
  */
-searchGame.Grid.prototype.reset = function() {
+searchGame.GridDOM.prototype.reset = function() {
 	// create tile array
 	this.tiles = squishy.createArray(this.height);
 	for (var j = 0; j < this.height; ++j) {
